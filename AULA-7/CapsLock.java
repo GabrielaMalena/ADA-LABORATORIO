a01cdab071594e0e52b11da74177342a113c06e6
Gabriela Quispe <gquispequispe@unsa.edu.pe>
2021-11-22 00:09:19 -0600

    falta implementar

--- AULA-7/CapsLock.java
+++ AULA-7/CapsLock.java
@@ -1,6 +1,22 @@
 
 public class CapsLock {
     public static void main(String[] args){ 
+        System.out.println ("Ingrese un string:");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        char[] s = str.toCharArray();
+                 System.out.print ("Letras minusculas:");
+        for (char s1:s) {
+            if(Character.isLowerCase(s1))
+            System.out.print(s1+" ");
+        }
+        System.out.println();
+                 System.out.print ("Letras mayusculas:");
+        for (char s2:s) {
+            if (Character.isUpperCase(s2))
+                System.out.print(s2+" ");
+        }
+        System.out.println();
     
      } 
 
