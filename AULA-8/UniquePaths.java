08fb4d50a24807a615917aa43fbdfed9181725a5
Gabriela Quispe <gquispequispe@unsa.edu.pe>
2021-12-03 23:59:32 -0600

    Se modifico Unique Paths

--- AULA-8/UniquePaths.java
+++ AULA-8/UniquePaths.java
@@ -1,35 +1,29 @@
 import java.util.*;
 class UniquePaths {
-	int[][] prueba; 
-
-	// metodo para el camino con obstaculos
-	public int uniquePathsWithObstacles(int[][] cuadricula) {
-		if(cuadricula == null || cuadricula.length == 0)
-			return 0;
-
-		prueba = new int[cuadricula.length][cuadricula[0].length];
-		return uniquePaths(cuadricula, 0, 0);
+    public int uniquePathsWithObstacles(int[][] cuadricula) {
+	int x = cuadricula.length;
+	int y = cuadricula[0].length;
+	if(cuadricula[0][0] == 1) return 0;
+	for(int i = 0; i < x; i++) {
+	    for(int j = 0; j < y; j++) {
+		if(i == 0 || j ==0) { // calculamos para la 1fila-1colum
+		    if(cuadricula[i][j] == 1 || (i!=0 && cuadricula[i-1][0] == 0) || (j!=0 && cuadricula[i][j-1] == 0)) { // obstaculos
+			cuadricula[i][j] = 0;
+		     }
+		     else {
+			 cuadricula[i][j] = 1;
+		     }
+		  }
+		  else {
+		       if(cuadricula[i][j] == 1) { // calculamos para las demas celdas
+			   cuadricula[i][j] = 0;
+			}
+			else {
+			    cuadricula[i][j] = cuadricula[i-1][j] + cuadricula[i][j-1];
+			}
+		     }
+		   }
+		}
+		return cuadricula[x-1][y-1];
 	}
-
-
-	// metodo para el unico camino sin obstaculos
-	private int uniquePaths(int[][] cuadricula, int x, int y){
-		if(x >= cuadricula[0].length || y >= cuadricula.length || cuadricula[y][x] == 1)
-			return 0;
-
-		else if(x == cuadricula[0].length - 1 && y == cuadricula.length - 1)
-			return 1;
-
-		else if(prueba[y][x] > 0)
-			return prueba[y][x];
-
-		return cuadricula[y][x] = uniquePaths(cuadricula, x + 1, y) + uniquePaths(cuadricula, x, y + 1);
-	}
-
-
-	public static void main(String[]args){
-		UniquePaths uniquePaths= new UniquePaths();
-		System.out.println(uniquePaths.uniquePathsWithObstacles(new int [3][2]));
-
-	} 
 }
