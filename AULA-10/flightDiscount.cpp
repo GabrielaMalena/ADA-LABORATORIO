7b5ad467f305699a91f2fee187fe1609e8cc1666
GabrielaMalena <83074491+GabrielaMalena@users.noreply.github.com>
2021-12-18 19:11:29 -0600

    se realizo cambios

--- AULA-10/flightDiscount.cpp
+++ AULA-10/flightDiscount.cpp
@@ -1,66 +1,90 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using std::cout;
-using std::endl;
-using std::vector;
-using std::pair;
-
-vector<long long> minimoCosto(
-	int from, const vector<vector<pair<int, int>>>& lado
-) {
-	vector<long long> minimoCosto(lado.size(), INT64_MAX);
-	minimoCosto[from] = 0;
-	std::priority_queue<pair<long long, int>> frente;
-	frente.push({0, from});
-	while (!frente.empty()) {
-		pair<long long, int> estadoActual = frente.top();
-		frente.pop();
-		int actual = estadoActual.segundo;
-		if (-estadoActual.first != minimoCosto[actual]) {
-			continue;
-		}
-		
-		for (auto [n, nc] : lado[actual]) {
-			long long nuevo = minimoCosto[actual] + nc;
-			if (nuevo < minimoCosto[n]) {
-				minimoCosto[n] = nuevo;
-				frente.push({-nuevo, n});
-			}
-		}
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+#define int long long int
+ 
+const int INF = 9e17; 
+int n, m;
+vector<vector<pair<int,int>>> g;
+vector<int> dist; 
+vector<int> disc;
+ 
+void dijkstra(){
+	priority_queue< 
+	pair<int, pair<int,int>>, 
+	vector<pair<int, pair<int,int>>>,
+	greater<pair<int, pair<int,int>>>
+	> pq;
+	for(int i = 2; i <= n; ++i)
+	{
+		dist[i] = INF; 
+		disc[i] = INF;
 	}
-	return minimoCosto;
-}
-
-int main() {
-	int numCiudad;
-	int numVuelo;
-	std::cin >> numCiudad >> numVuelo;
-	vector<vector<pair<int, int>>> lado(numCiudad);
-	vector<vector<pair<int, int>>> ladoOpuesto(numCiudad);
-	for (int f = 0; f < numVuelo; f++) {
-		int from;
-		int to;
-		int cost;
-		std::cin >> from >> to >> cost;
-		lado[--from].push_back({--to, cost});
-		ladoOpuesto[to].push_back({from, cost});
+	pq.push({0,{1,0}});
+ 
+	while(!pq.empty())
+	{
+		int d = pq.top().first;
+		int u = pq.top().second.first;
+		int f = pq.top().second.second;
+		pq.pop();
+ 
+		if(f == 1)
+		{
+			if(disc[u] < d) continue; 
+		}
+ 
+		if(f == 0)
+		{
+			if(dist[u] < d) continue; 
+		}
+ 
+		for(auto e: g[u])
+		{
+			int v = e.first, c = e.second;
+			if(f == 0) 
+			{
+				if(dist[v] > c + d) 
+				{
+					dist[v] = c+d;
+					pq.push({dist[v], {v,0}});
+				}
+				if(disc[v] > d + c/2) 
+				{
+					disc[v] = d + c/2;
+					pq.push({disc[v], {v,1}});	
+				}
+			}
+ 
+			if(f==1)
+			{
+				if(disc[v] > d + c)
+				{
+					disc[v] = d + c;
+					pq.push({disc[v], {v,1}});	
+				}
+			}
+ 
+		}
 	}
  
-	vector<long long> costo1 = minimoCosto(0, lado);
-	vector<long long> costo2 = minimoCosto(numCiudad - 1, ladoOpuesto);
-	long long total = INT64_MAX;
-	for (int c = 0; c < numCiudad; c++) {
-		for (auto [n, nc] : lado[c]) {
-			if (costo1[c] == INT64_MAX || costo2[n] == INT64_MAX) {
-				continue;
-			}
-			total = std::min(
-				total,
-				costo1[c] + (nc / 2) + costo2[n]
-			);
-		}
+	cout << disc[n] << endl;
+}
+ 
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+ 
+	cin >> n >> m;
+	g.resize(n+1);
+	dist.resize(n+1);
+	disc.resize(n+1);
+	for(int i = 0; i < m; ++i)
+	{
+		int u, v, c;
+		cin >> u >> v >> c;
+		g[u].push_back({v,c});
 	}
-	cout << total << endl;
+ 
+	dijkstra();
 }
